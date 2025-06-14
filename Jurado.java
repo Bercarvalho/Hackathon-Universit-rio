95f92ae479e402f3fa54f448de1f2bd2a046fb04
João Gabriel da Silva Moreira <145884285+WoaGabriel@users.noreply.github.com>
2025-06-14 16:59:03 -0300

    Add files via upload
    Classe Jurados

---
+++ Jurado.java
@@ -0,0 +1,17 @@
+public class Jurado {
+    private String nome;
+    private String area;
+
+    public Jurado(String nome, String area) {
+        this.nome = nome;
+        this.area = area;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getArea() {
+        return area;
+    }
+}
